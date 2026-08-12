f0bdc67ed60331ba6564806c288e28b7e8b2258d
Ana-Maria14 <anamariacatrina14@gmail.com>
2026-08-12 16:42:26 +0300

    Update script

--- Lucid_Escape_Game/Assets/Echipa/Ana-camera4/PlayerMovement.cs
+++ Lucid_Escape_Game/Assets/Echipa/Ana-camera4/PlayerMovement.cs
@@ -6,14 +6,14 @@ public class PlayerMovement : MonoBehaviour
     public float mouseSensitivity = 2f; 
     
     // Setări noi pentru săritură
-    public float jumpForce = 8f; // Forța maximă a săriturii (dacă ții apăsat)
-    public float gravity = -20f; // Gravitația (mai mare ca să cazi înapoi realist, nu ca pe lună)
+    public float jumpForce = 8f;
+    public float gravity = -20f;
 
     private float xRotation = 0f;
     private Transform playerCamera;
     private CharacterController controller;
     
-    private float velocityY = 0f; // Memorează viteza cu care urci/cazi
+    private float velocityY = 0f;
 
     void Start()
     {
@@ -24,7 +24,6 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        // --- 1. ROTIREA CAMEREI ---
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
 
@@ -34,41 +33,30 @@ public class PlayerMovement : MonoBehaviour
         playerCamera.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         transform.Rotate(Vector3.up * mouseX);
 
-        // --- 2. MIȘCAREA ORIZONTALĂ (W, A, S, D) ---
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
-        // Înmulțim direct aici direcția cu viteza
         Vector3 move = (transform.right * x + transform.forward * z) * speed;
 
-        // --- 3. GRAVITAȚIA ȘI SĂRITURA DINAMICĂ ---
-        
-        // Dacă suntem pe pământ, oprim gravitația să nu ne tragă la infinit în jos
         if (controller.isGrounded && velocityY < 0)
         {
             velocityY = -2f; 
         }
 
-        // Când APESI Space -> Începe săritura maximă
         if (Input.GetKeyDown(KeyCode.Space) && controller.isGrounded)
         {
             velocityY = jumpForce;
         }
 
-        // Când ELIBEREZI Space -> Magia dinamică!
-        // Dacă te afli în aer și încă urci (velocityY > 0), tăiem din viteză
         if (Input.GetKeyUp(KeyCode.Space) && velocityY > 0)
         {
-            velocityY *= 0.4f; // Cu cât numărul e mai mic, cu atât se oprește mai brusc
+            velocityY *= 0.4f;
         }
 
-        // Aplicăm mereu gravitația ca să fim trași în jos
         velocityY += gravity * Time.deltaTime;
 
-        // Punem viteza verticală în mișcarea finală
         move.y = velocityY;
-
-        // Mutăm caracterul cu totul
+        
         controller.Move(move * Time.deltaTime);
     }
 }
